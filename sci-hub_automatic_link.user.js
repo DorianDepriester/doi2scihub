8fd27b0aabdfb68bcd81ac4e06f0235180b8f57b
Dorian Depriester <dorian@crans.org>
2016-05-13 11:56:56 +0200

    Add files via upload

---
+++ sci-hub_automatic_link.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        sci-hub automatic link
+// @namespace   http://blog.dorian-depriester.fr
+// @description Automatically turns each DOI link into a new link to the corresponding document on sci-hub.cc
+// @version     1
+// @grant       none
+// ==/UserScript==
+var els = document.getElementsByTagName("A");
+for(var i = 0, l = els.length; i < l; i++) {
+    var el = els[i];
+    el.href = el.href.replace(\S*(\/\/)*dx\.doi\.org\//gmi, 'http://sci-hub.cc/');
+}
