dabdf10b0df781ea91865c5690a1b4de3ab72ec2
Dorian Depriester <dorian@crans.org>
2017-03-21 17:05:30 +0100

    Wait for other scripts to finishing before executing
    The script now waits for the end of all running scripts (e.g. jQuery) before parsing. Now works on €l$evier website.

--- sci-hub_automatic_link.user.js
+++ sci-hub_automatic_link.user.js
@@ -2,11 +2,13 @@
 // @name        sci-hub automatic link
 // @namespace   http://blog.dorian-depriester.fr
 // @description Automatically turns each DOI link into a new link to the corresponding document on sci-hub.cc
-// @version     1
+// @version     3
 // @grant       none
 // ==/UserScript==
-var els = document.getElementsByTagName("A");
-for(var i = 0, l = els.length; i < l; i++) {
-    var el = els[i];
-    el.href = el.href.replace(/^((http(s|):\/\/|www\.)|)(dx.|)doi\.org\//gim, 'http://sci-hub.cc/');
-}
+window.addEventListener('load', function() {
+    var els = document.getElementsByTagName("A");
+    for(var i = 0, l = els.length; i < l; i++) {
+       var el = els[i];
+       el.href = el.href.replace(/^((http(s|):\/\/|www\.)|)(dx.|)doi\.org\//gim, 'http://sci-hub.cc/');
+    }
+}, false);   
