45dd301b2cbb04b1ee41f70178f7f6d641b706a6
Dorian Depriester <dorian@crans.org>
2017-03-31 16:06:29 +0200

    'addEventListener' is back
    Using '@run-at  document-idle' is not enough on sciencedirect, hence it appears necessary to reimplment the window.addEventListener() routine (only works on Firefox/Greasemonkey)

--- sci-hub_automatic_link.user.js
+++ sci-hub_automatic_link.user.js
@@ -5,13 +5,15 @@
 // @description Automatically turns each DOI link into a new link to the corresponding document on sci-hub.cc
 // @downloadURL https://github.com/DorianDepriester/doi2scihub/raw/master/sci-hub_automatic_link.user.js
 // @supportURL  https://github.com/DorianDepriester/doi2scihub/issues
-// @version     4
+// @version     5
 // @grant       none
 // @match       *://*/*
 // @run-at      document-idle
 // ==/UserScript==
-var els = document.getElementsByTagName("A");
-for(var i = 0, l = els.length; i < l; i++) {
-   var el = els[i];
-   el.href = el.href.replace(/^((http(s|):\/\/|www\.)|)(dx.|)doi\.org\//gim, 'http://sci-hub.cc/');
-} 
+window.addEventListener('load', function() {
+   var els = document.getElementsByTagName("A");
+   for(var i = 0, l = els.length; i < l; i++) {
+      var el = els[i];
+      el.href = el.href.replace(/^((http(s|):\/\/|www\.)|)(dx.|)doi\.org\//gim, 'http://sci-hub.cc/');
+   }
+}, false);
