ffee63359fe4e33c32d5b1f190378931a4358d0e
Dorian Depriester <dorian@crans.org>
2017-03-31 15:43:33 +0200

    Use of 'run-at' instead of 'addEventListener'
    This may fix the unexpected reload issue (see issue #2) on some websites
    + Additional meta-data

--- sci-hub_automatic_link.user.js
+++ sci-hub_automatic_link.user.js
@@ -1,15 +1,17 @@
 // ==UserScript==
 // @name        sci-hub automatic link
-// @namespace   http://blog.dorian-depriester.fr
+// @website     http://blog.dorian-depriester.fr
+// @author      Dorian Depriester
 // @description Automatically turns each DOI link into a new link to the corresponding document on sci-hub.cc
-// @version     3
+// @downloadURL https://github.com/DorianDepriester/doi2scihub/raw/master/sci-hub_automatic_link.user.js
+// @supportURL  https://github.com/DorianDepriester/doi2scihub/issues
+// @version     4
 // @grant       none
-// @match      *://*/*
+// @match       *://*/*
+// @run-at      document-idle
 // ==/UserScript==
-window.addEventListener('load', function() {
-    var els = document.getElementsByTagName("A");
-    for(var i = 0, l = els.length; i < l; i++) {
-       var el = els[i];
-       el.href = el.href.replace(/^((http(s|):\/\/|www\.)|)(dx.|)doi\.org\//gim, 'http://sci-hub.cc/');
-    }
-}, false);   
+var els = document.getElementsByTagName("A");
+for(var i = 0, l = els.length; i < l; i++) {
+   var el = els[i];
+   el.href = el.href.replace(/^((http(s|):\/\/|www\.)|)(dx.|)doi\.org\//gim, 'http://sci-hub.cc/');
+} 
